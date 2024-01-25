2c4844d67368bcc716892798014b94a2b67802bc
PhilomenaMusili <philomenamusili2@gmail.com>
2024-01-25 05:04:43 -0800

    added bootstrap5 to style computerentry page

--- computerInventory/settings.py
+++ computerInventory/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'inventoryapp',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
