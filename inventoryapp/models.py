530baadf33211e916aac18b514cd4ad7841e34e4
PhilomenaMusili <philomenamusili2@gmail.com>
2024-01-25 05:18:46 -0800

    Added a new field for purchase date

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime, date
 # Create your models here.
 
 class Computer(models.Model):
@@ -8,6 +8,8 @@ class Computer(models.Model):
     MAC_address = models.CharField(max_length=30)
     users_name = models.CharField(max_length=30)
     location = models.CharField(max_length=30)
+    purchase_date=models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    timestamp=models.DateField(auto_now_add=True, auto_now=False, blank=True)
 
 def __unicode__(self):
     return self.IP_address + ' ' + self.computer_name
