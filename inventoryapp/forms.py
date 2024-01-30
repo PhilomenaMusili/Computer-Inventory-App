c82360d2252463f78b38794ecbcc544d00edda28
PhilomenaMusili <philomenamusili2@gmail.com>
2024-01-30 10:48:16 +0300

    Added computer search form

--- inventoryapp/forms.py
+++ inventoryapp/forms.py
@@ -5,3 +5,8 @@ class ComputerForm(forms.ModelForm):
    class Meta:
      model = Computer
      fields = ['computer_name', 'IP_address', 'MAC_address', 'users_name', 'location', 'purchase_date']
+
+class ComputerSearchForm(forms.ModelForm):
+class Meta:
+model = Computer
+fields = [‘computer_name’, ‘users_name’]
