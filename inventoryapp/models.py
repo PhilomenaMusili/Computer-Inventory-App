5918a5656de9533149667e1bbf2e7fd8e9098356
PhilomenaMusili <philomenamusili2@gmail.com>
2024-01-30 15:47:45 +0300

    multiple choice field and linking table

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -1,23 +1,22 @@
 from django.db import models
-from datetime import datetime, date
-# Create your models here.
+
+class OperatingSystem(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 class Computer(models.Model):
     computer_name = models.CharField(max_length=30, blank=True)
     IP_address = models.CharField(max_length=30, blank=True)
     MAC_address = models.CharField(max_length=30, blank=True)
-    os_choice = (
-            ('Windows 11', 'Windows 11'),
-            ('Windows 10', 'Windows 10'),
-            ('Windows 8', 'Windows 8'),
-            ('Linux', 'Linux'),
-        )
-    operating_system = models.CharField(max_length=30, blank=True, null=True, choices=os_choice)
+
+    operating_system = models.ManyToManyField(OperatingSystem, blank=True)
 
     users_name = models.CharField(max_length=30, blank=True)
     location = models.CharField(max_length=30, blank=True)
-    purchase_date=models.DateField("Purchase Date(mm/dd/2023)",auto_now_add=False, auto_now=False, blank=True, null=True)
-    timestamp=models.DateField(auto_now_add=True, auto_now=False, blank=True)
+    purchase_date = models.DateField("Purchase Date (mm/dd/2023)", auto_now_add=False, auto_now=False, blank=True, null=True)
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
 
-def __unicode__(self):
-    return self.IP_address + ' ' + self.computer_name
+    def __str__(self):
+        return f"{self.computer_name} ({', '.join(os.name for os in self.operating_system.all())})"
