38b293425ef96c85e7e839d1105d6a73a0c2781e
PhilomenaMusili <philomenamusili2@gmail.com>
2024-01-30 14:49:53 +0300

    Added blank=True

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -4,10 +4,10 @@ from datetime import datetime, date
 
 class Computer(models.Model):
     computer_name = models.CharField(max_length=30, blank=True)
-    IP_address = models.CharField(max_length=30)
-    MAC_address = models.CharField(max_length=30)
+    IP_address = models.CharField(max_length=30, blank=True)
+    MAC_address = models.CharField(max_length=30, blank=True)
     users_name = models.CharField(max_length=30, blank=True)
-    location = models.CharField(max_length=30)
+    location = models.CharField(max_length=30, blank=True)
     purchase_date=models.DateField("Purchase Date(mm/dd/2023)",auto_now_add=False, auto_now=False, blank=True, null=True)
     timestamp=models.DateField(auto_now_add=True, auto_now=False, blank=True)
 
