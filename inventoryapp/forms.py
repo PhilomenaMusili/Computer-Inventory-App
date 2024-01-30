e2deb0efc657dc8060fb0a9d2b13b01ba343d870
PhilomenaMusili <philomenamusili2@gmail.com>
2024-01-30 14:51:16 +0300

    Form validation

--- inventoryapp/forms.py
+++ inventoryapp/forms.py
@@ -5,7 +5,16 @@ class ComputerForm(forms.ModelForm):
     class Meta:
         model = Computer
         fields = ['computer_name', 'IP_address', 'MAC_address', 'users_name', 'location', 'purchase_date']
-
+def clean_computer_name(self): # Validates the Computer Name Field
+	computer_name = self.cleaned_data.get('computer_name')
+	if (computer_name == ""):
+		raise forms.ValidationError('This field cannot be left blank')
+	return computer_name
+def clean_IP_address(self): 
+	IP_address = self.cleaned_data.get('IP_address')
+	if (IP_address == ""):
+		raise forms.ValidationError('This field cannot be left blank')
+	return IP_address
 class ComputerSearchForm(forms.ModelForm):
     class Meta:
         model = Computer
