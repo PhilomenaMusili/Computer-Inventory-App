db8ec512a19d10fa21fd3445d54eaf1d9bc206a7
PhilomenaMusili <philomenamusili2@gmail.com>
2024-02-23 17:09:01 +0300

    modified:   computerInventory/settings.py

--- computerInventory/settings.py
+++ computerInventory/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'computerInventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'computerinventory',
+        'USER': 'root',
+        'PASSWORD': 'Password@12',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
